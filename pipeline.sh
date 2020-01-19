357fe9655a417f79eb0d63422d5b611ba2239d8c
Alos Diallo <alos31@gmail.com>
2020-01-19 17:44:10 -0500

    counts is incorrect case
    Changed counts to Counts

--- pipeline.sh
+++ pipeline.sh
@@ -17,7 +17,7 @@ STAR --runThreadN 12 --genomeDir $PWD/Index/ --outFileNamePrefix $PWD/STARResult
 samtools sort -n $PWD/STARResults/$base.Aligned.out.sam -o $PWD/SortedResults/$base.sorted.sam
 
 #@3,2,htseqcount,,sbatch -p short -n 1 -t 20:0 --mem 5G
-htseq-count -m union -r name -i gene_name -a 10 --stranded=no $PWD/SortedResults/$base.sorted.sam $PWD/genome/Mus_musculus.GRCm38.97.gtf > $PWD/counts/$base.counts
+htseq-count -m union -r name -i gene_name -a 10 --stranded=no $PWD/SortedResults/$base.sorted.sam $PWD/genome/Mus_musculus.GRCm38.97.gtf > $PWD/Counts/$base.counts
 
 
 done
