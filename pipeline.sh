a114e86fb4b29af5e33456143935d5dda46e0128
Alos Diallo <alos31@gmail.com>
2020-01-19 19:45:56 -0500

    Memory and time were not enough
    I increased the ram and time for the jobs.  Especially for the counts.

--- pipeline.sh
+++ pipeline.sh
@@ -8,15 +8,15 @@ do
 
 #create a prefix
 base=`basename $fastq .fastq`
-#@1,0,star,,sbatch -p short -n 1 -t 20:0 --mem 30G -c 13
+#@1,0,star,,sbatch -p short -n 1 -t 60:0 --mem 50G -c 13
 STAR --runThreadN 12 --genomeDir $PWD/Index/ --outFileNamePrefix $PWD/STARResults/$base. --readFilesIn $fastq
 
 
 
-#@2,1,Samt,,sbatch -p short -n 1 -t 20:0 --mem 30G -c 10
+#@2,1,Samt,,sbatch -p short -n 1 -t 60:0 --mem 50G -c 10
 samtools sort -n $PWD/STARResults/$base.Aligned.out.sam -o $PWD/SortedResults/$base.sorted.sam
 
-#@3,2,htseqcount,,sbatch -p short -n 1 -t 20:0 --mem 5G
+#@3,2,htseqcount,,sbatch -p short -n 1 -t 60:0 --mem 50G
 htseq-count -m union -r name -i gene_name -a 10 --stranded=no $PWD/SortedResults/$base.sorted.sam $PWD/genome/Mus_musculus.GRCm38.97.gtf > $PWD/Counts/$base.counts
 
 
