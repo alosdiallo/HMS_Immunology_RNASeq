86a4f9eeeae94ac6f816fa1e9b46f2f425d50a85
Alos Diallo <alos31@gmail.com>
2020-07-23 13:05:13 -0400

    Update pipeline.sh
    fixed spacing

--- pipeline.sh
+++ pipeline.sh
@@ -11,8 +11,6 @@ base=`basename $fastq .fastq`
 #@1,0,star,,sbatch -p short -n 1 -t 60:0 --mem 50G -c 13
 STAR --runThreadN 12 --genomeDir $PWD/Index/ --outFileNamePrefix $PWD/STARResults/$base. --readFilesIn $fastq
 
-
-
 #@2,1,Samt,,sbatch -p short -n 1 -t 60:0 --mem 50G -c 10
 samtools sort -n $PWD/STARResults/$base.Aligned.out.sam -o $PWD/SortedResults/$base.sorted.sam
 
