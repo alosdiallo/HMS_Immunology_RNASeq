f6da291a6de13d429e5050f5c5e5cc966c55a522
Alos Diallo <alos31@gmail.com>
2021-01-10 22:06:05 -0500

    Update sorter.sh

--- sorter.sh
+++ sorter.sh
@@ -1,11 +1,12 @@
 
 #!/bin/sh
 
-mkdir -p STARResults SortedResults  Counts
-
 for bam in $PWD/*.bam
 
-#@2,1,Samt,,sbatch -p short -n 1 -t 60:0 --mem 50G -c 10
-samtools sort -n $PWD/STARResults/$base.Aligned.out.bam -o $PWD/SortedResults/$base.sorted.bam
+do
+
+#create a prefix
+#@1,0,Samt,,sbatch -p short -n 1 -t 60:0 --mem 50G -c 10
+samtools sort -n "$bam" -o "$bam".sorted.bam
 
 done
