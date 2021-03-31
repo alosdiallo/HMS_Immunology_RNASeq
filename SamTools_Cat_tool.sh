aace8df196e6e443df45cc84f7808689c2283e3e
Alos Diallo <alos31@gmail.com>
2021-03-31 17:26:27 -0400

    Update SamTools_Cat_tool.sh

--- SamTools_Cat_tool.sh
+++ SamTools_Cat_tool.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#runAsPipeline SamTools_Cat_tool.sh "sbatch -p short -t 20:0 -n 1" noTmp run
+#runAsPipeline Cat_fastq.sh "sbatch -p short -t 20:0 -n 1" noTmp run
 #modules needed:
 #1) gcc/6.2.0   2) bcbio/latest   3) samtools/1.10   4) rcbio/1.1
 for lane1 in *L001*;
@@ -7,10 +7,9 @@ for lane1 in *L001*;
 do 
 
 #create a prefix
-sample=`basename $lane1 L001_R1.Aligned.out.bam` 
+sample=$(echo "$lane1" | cut -f 1,2,3 -d '_'); 
 
 #@1,0,samFileCat,,sbatch -p short -n 1 -t 60:0 --mem 20G -c 1
-samtools cat -o "$sample"R1.Aligned.out.bam "$sample"L001_R1.Aligned.out.bam "$sample"L002_R1.Aligned.out.bam "$sample"L003_R1.Aligned.out.bam "$sample"L004_R1.Aligned.out.bam
-samtools cat -o "$sample"R2.Aligned.out.bam "$sample"L001_R2.Aligned.out.bam "$sample"L002_R2.Aligned.out.bam "$sample"L003_R2.Aligned.out.bam "$sample"L004_R2.Aligned.out.bam
+samtools cat -o "$sample"_R1.Aligned.out.bam "$sample"_L001_R1.Aligned.out.bam "$sample"_L002_R1.Aligned.out.bam "$sample"_L003_R1.Aligned.out.bam "$sample"_L004_R1.Aligned.out.bam
 
 done
