76bf421614898ae9c284c3c35fd1c075814fbb9a
Alos Diallo <alos31@gmail.com>
2021-12-13 22:20:33 -0500

    updated the file
    added info on the new location of the gtf and strand

--- counter.sh
+++ counter.sh
@@ -5,6 +5,6 @@ for sam in $PWD/*.sam
 do
 
 #@1,0,htseqcount,,sbatch -p short -n 1 -t 60:0 --mem 50G
-htseq-count -m union -r name -i gene_name -a 10 --stranded=no "$sam" /home/ad249/immdiv-bioinfo/karni/mouse_genome/Mus_musculus.GRCm38.97.gtf > "$sam".counts
+htseq-count -m union -r name -i gene_name -a 10 --stranded=yes "$sam" /n/shared_db/hg19/uk/star/2.5.4a/genes.gtf > "$sam".counts
 
 done
